6f9a3b2215453f584b8f125492bfbc0c291de209
TheodoreBillotte <theodore.billotte@epitech.eu>
2026-04-06 22:47:11 +0800

    add all the assets into the static folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,42 +15,42 @@
 <nav class="fixed top-0 z-300 flex w-full flex-row items-center justify-end gap-3 bg-[#A7E257] p-4 px-8">
   <a href="https://x.com/ninesliced" class="flex flex-row gap-2 items-center">
     <img
-      src="src/lib/assets/page/logos/twitter.svg"
+      src="/assets/page/logos/twitter.svg"
       alt="Twitter"
       class="h-10"
     />
   </a>
   <a href="https://bsky.app/profile/ninesliced.com" class="flex flex-row gap-2 items-center">
     <img
-      src="src/lib/assets/page/logos/bluesky.svg"
+      src="/assets/page/logos/bluesky.svg"
       alt="Bluesky"
       class="h-10"
     />
   </a>
   <a href="https://instagram.com/ninesliced_games" class="flex flex-row gap-2 items-center">
     <img
-      src="src/lib/assets/page/logos/instagram.svg"
+      src="/assets/page/logos/instagram.svg"
       alt="Instagram"
       class="h-10"
     />
   </a>
   <a href="https://youtube.com/@ninesliced_games" class="flex flex-row gap-2 items-center">
     <img
-      src="src/lib/assets/page/logos/youtube.svg"
+      src="/assets/page/logos/youtube.svg"
       alt="Youtube"
       class="h-10"
     />
   </a>
 
-  <a href="#games" class="hidden sm:inline-block top-0 left-0 text-2xl font-bold px-2">
+  <a href="/#games" class="hidden sm:inline-block top-0 left-0 text-2xl font-bold px-2">
     <OutlinedText textSize="1.5rem" strokeWidth="6px">Games</OutlinedText>
   </a>
 
-  <a href="#modding" class="hidden sm:inline-block top-0 left-0 text-2xl font-bold px-2">
+  <a href="/#modding" class="hidden sm:inline-block top-0 left-0 text-2xl font-bold px-2">
     <OutlinedText textSize="1.5rem" strokeWidth="6px">Modding</OutlinedText>
   </a>
 
-  <a href="#about" class="hidden sm:inline-block top-0 left-0 text-2xl font-bold px-2">
+  <a href="/#about" class="hidden sm:inline-block top-0 left-0 text-2xl font-bold px-2">
     <OutlinedText textSize="1.5rem" strokeWidth="6px">About</OutlinedText>
   </a>
 </nav>
